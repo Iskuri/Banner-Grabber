92342cb230ee475df8c43effaf7451406ff9e71b
christopher <christopher@desktop.laptop>
2013-09-28 17:07:21 +0100

    timeouts...

--- CurlHandler.py
+++ CurlHandler.py
@@ -8,17 +8,20 @@ class CurlHandler:
 	def getResponse(self, ip_address):
 
 		c = pycurl.Curl()
-
-		c.setopt(c.URL, 'https://'+ip_address)
-		buf = cStringIO.StringIO()
-		c.setopt(c.WRITEFUNCTION, buf.write)
-		c.setopt(c.SSL_VERIFYPEER, 0)
-		c.setopt(c.SSL_VERIFYHOST, 0)
-		c.setopt(c.NOSIGNAL, 1)
-		c.setopt(c.CONNECTTIMEOUT, 5)
-    		c.setopt(c.TIMEOUT, 5)
-		c.perform()
-		response = buf.getvalue()
-		buf.close()
-		#print response
-		return response
+		
+		try:
+			c.setopt(c.URL, 'https://'+ip_address)
+			buf = cStringIO.StringIO()
+			c.setopt(c.WRITEFUNCTION, buf.write)
+			c.setopt(c.SSL_VERIFYPEER, 0)
+			c.setopt(c.SSL_VERIFYHOST, 0)
+			c.setopt(c.NOSIGNAL, 1)
+			c.setopt(c.CONNECTTIMEOUT, 5)
+    			c.setopt(c.TIMEOUT, 5)
+			c.perform()
+			response = buf.getvalue()
+			buf.close()
+			#print response
+			return response
+		except:
+			return ''
